d5a572b670564b93a278ddcf30bb428ec372009a
adriangohjw <adriangohjw@gmail.com>
2021-12-01 13:05:28 +0800

    Add functionality to create new record

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,25 @@ class ApplicationController < ActionController::Base
     @links = Link.order(created_at: :desc).last(100)
   end
 
+  def create
+    @link = Link.new(create_params)
+    @link.save!
+  rescue StandardError => e
+    flash.notice = e.message
+  ensure
+    redirect_to root_path
+  end
+
   def to
     @link = Link.find_by_shortened_path(params[:shortened_path])
     redirect_to @link.original_url, status: :moved_permanently
   rescue StandardError
     redirect_to root_path, notice: 'Link not found'
+  end
+
+  private
+
+  def create_params
+    params.permit(:original_url, :shortened_path)
   end
 end
