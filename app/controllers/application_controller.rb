3185c5618b752e866732abd7750c31577f14949c
adriangohjw <adriangohjw@gmail.com>
2021-12-01 12:47:52 +0800

    Add ability to view and redirect with shortener

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  def index
+    @links = Link.order(created_at: :desc).last(100)
+  end
+
+  def to
+    @link = Link.find_by_shortened_path(params[:shortened_path])
+    redirect_to @link.original_url, status: :moved_permanently
+  rescue StandardError
+    redirect_to root_path, notice: 'Link not found'
+  end
 end
